678c70b3dc8439f346a7f4d6ecdcd94e0c167073
Ryan Showalter <showalrm@dukes.jmu.edu>
2022-04-06 17:31:59 -0400

    Initial commit

---
+++ src/Bird.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { Card } from 'react-bootstrap';
+
+function Bird() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("http://demo.jmunixusers.org:7777")
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+  }, [])
+
+  if (!data.name) {
+      return <p>Error</p>;
+  }
+
+  return (
+        <Card style={{ width: '18rem'}}>
+        <Card.Img variant="top" src={data.image} className="rounded mx-auto d-block" alt="a bird"/>
+            <Card.Body>
+                <Card.Title>
+                    {data.name}
+                </Card.Title>
+            </Card.Body>
+          
+      </Card>
+  )
+}
+
+export default Bird;
